a1edec564270fb6762489d2817e542d4bfd79aff
Developer <m_mannaa2008@yahoo.com>
2020-09-19 12:02:12 +0300

    Add Models - View & url to my Project

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,26 @@ from django.utils import timezone
 # Create your models here.
 
 
+POST_TYPE =(
+    ('DRAFT','DRAFT'),
+    ('PUBLISHED','PUBLISHED'),
+)
+
 class Post(models.Model): # db table
-    title = models.CharField(max_length = 50) # column
-    content = models.TextField(max_length = 2000) # column
-    create_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length = 50, unique=True) # column
+    content = models.TextField(max_length = 2000, blank=True, null=True) # column
+    create_at = models.DateTimeField(default=timezone.now) # column
+    active = models.BooleanField(default=False) # column
+    author_mail = models.EmailField(default='') # column
+    type = models.CharField(choices=POST_TYPE, max_length=30, default='DRAFT') # column
+    image = models.ImageField(upload_to='post/')
+    
+    
+    
     
     
     
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
     
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post 
 
 # Create your views here.
+
+def all_posts(request):
+    ## Logic
+    all_posts = Post.objects.all()
+    return render(request,'post/all_posts.html' ,{'posts':all_posts})
+
+def single_post(request,id):
+    ## Logic
+    single_post = Post.objects.get(id=id)
+    return render(request,'post/single_post.html' ,{'post':single_post})    
